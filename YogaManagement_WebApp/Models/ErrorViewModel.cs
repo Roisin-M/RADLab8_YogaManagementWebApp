461631a7dce12dc571d32ca368dcfe3941b79646
roisin-m <s00223865@atu.ie>
2025-01-02 14:18:45 +0000

    removed webApp

--- YogaManagement_WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace YogaManagement_WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
